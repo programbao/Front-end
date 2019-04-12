22343994dae4fe8167716f6a3c4a335f0ce64a03
xiaoxiaobao <42453793+xiaoxiaobao@users.noreply.github.com>
2019-04-12 23:18:17 +0800

    点餐项
    完成积分页和点餐页，还有一些绑定事件没有处理

---
+++ Wechat-smallProgram-day5/pages/order/order.js
@@ -0,0 +1,71 @@
+// pages/order/order.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    products:["锅底类","丸滑类","特色菜类","经典火锅菜","牛肉羊肉类","海鲜河鲜类","豆面制品"]
+  },
+  
+  open:function(e){
+
+      console.log(e.currentTarget.dataset);
+  
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
